c0b480d9fd126eafe3132eadb1528eb504157cb4
shivafa <shiva.fd05@gmail.com>
2019-08-07 11:03:44 +0200

    adding bootstrap and angular material

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
