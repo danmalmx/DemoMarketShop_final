a2eb29fd3bf170bd7c91511f9ec40034fc44596c
danmalmx <danmalmx@gmail.com>
2019-08-20 19:39:48 +0200

    Adding password confirmation with messages

--- Angular/src/app/shared/user.service.ts
+++ Angular/src/app/shared/user.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
   formModel = this.fb.group({
-    UserName: [''],
+    UserName: ['', Validators.required],
+    Email: ['', Validators.email],
     FullName: [''],
     Passwords: this.fb.group({
-      Password: [''],
-      ConfirmPassword: ['']
-    })
-  })
+      Password: ['', [Validators.required, Validators.minLength(4)]],
+      ConfirmPassword: ['', Validators.required]
+    }, { validator: this.comparePasswords })
+  });
+  comparePasswords(fb: FormGroup) {
+    let passwordCtrl = fb.get('ConfirmPassword');
+    if (passwordCtrl.errors == null || 'passwordMismatch' in passwordCtrl.errors) {
+      if (fb.get('Password').value != passwordCtrl.value) {
+        passwordCtrl.setErrors({ passwordMismatch: true })
+      }
+      else {
+        passwordCtrl.setErrors(null);
+      }
+    }
+  }
 }
